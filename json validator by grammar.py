72f99b782aab2fcb280c89f4719aa7b12e8dc431
NataliaAssange <36265870+NataliaAssange@users.noreply.github.com>
2022-07-08 11:41:20 +0800

    Add files via upload
    It looks similar like those online json validators or parsers (I don't really know the difference between them since my English is poor)
    Please change my path into your own path
    If you're varifying an English file, just delete the "utf8"

---
+++ json validator by grammar.py
@@ -0,0 +1,25 @@
+import json
+import re
+try:
+    with open(r'C:\Users\CN0CHHG\Downloads\case_data_index.json',encoding="utf-8") as f:
+        a=f.read()
+        json.JSONDecoder().decode(a)
+except Exception as e:
+    print("错误信息:",e)
+    msg=str(e)
+    l=re.findall(r"\d+.?\d*",msg)
+    line=int(l[0])
+    char1=int(l[1])-6
+    char2=int(l[1])+4
+    print(type(char1))
+    #find line
+    with open(r'C:\Users\CN0CHHG\Downloads\case_data_index.json',encoding="utf-8") as f:
+        i = f.readline()
+        count = 1
+        while i:
+            if count >= line:
+                break
+            i = f.readline()
+            count += 1
+    #find char
+    print(i[char1:char2])
